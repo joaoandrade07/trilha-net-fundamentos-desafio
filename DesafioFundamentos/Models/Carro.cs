28f57eb92bba357b90b5efb4490e1ab40289c164
João Andrade <joaodeandrade07@gmail.com>
2025-07-03 02:55:48 -0300

    :bulb: docs: Comentando a classe carro

--- DesafioFundamentos/Models/Carro.cs
+++ DesafioFundamentos/Models/Carro.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace DesafioFundamentos.Models
 {
+    /// <summary>
+    /// Representa um carro com placa e hora de entrada
+    /// </summary>
     public class Carro
     {
         public string Placa { get; set; }
@@ -14,7 +17,11 @@ namespace DesafioFundamentos.Models
         public Carro()
         {
         }
-
+        /// <summary>
+        /// Contrutor da classe carro
+        /// </summary>
+        /// <param name="placa">Placa do carro</param>
+        /// <param name="horaEntrada">Hora de entrada do carro</param>
         public Carro(string placa, DateTime horaEntrada)
         {
             this.Placa = placa;
